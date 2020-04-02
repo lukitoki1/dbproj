068bbf00e1295f8e74e9fc9e093e229abaabe214
Łukasz Kamiński <lukasz.kaminski@empik.com>
2020-04-02 18:22:42 +0200

    Add table models and enable them in admin view

--- django/database_app/admin.py
+++ django/database_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Brand)
+admin.site.register(Car)
+admin.site.register(Hashtag)
+admin.site.register(Thread)
+admin.site.register(Comment)
--- django/database_app/models.py
+++ django/database_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nick = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+
+
+class Brand(models.Model):
+    brand = models.CharField(max_length=30)
+    model = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    production_begin = models.DateField()
+    production_end = models.DateField()
+
+
+class Car(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    production_date = models.DateField()
+    description = models.CharField(max_length=3000)
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=5000)
+    timestamp = models.DateTimeField()
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    hashtags = models.ManyToManyField(Hashtag)
+    nick = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=5000)
+    timestamp = models.DateTimeField()
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    nick = models.ForeignKey(User, on_delete=models.PROTECT)
