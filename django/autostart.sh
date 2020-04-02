29d508ca279adeb37debcc9410e7b7611eabde92
Łukasz Kamiński <lukasz.kaminski@empik.com>
2020-04-02 18:23:29 +0200

    Automatically create admin account and make migrations via startup script

--- django/autostart.sh
+++ django/autostart.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-sleep 7;
-python manage.py migrate &
+sleep 7 # waiting for postgres to come alive
+
+python manage.py makemigrations
+python manage.py migrate
+
+admin_nick="admin"
+admin_pass="admin"
+admin_email="admin@gmail.com"
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$admin_nick', '$admin_email', '$admin_pass')" | python manage.py shell
+
 python manage.py runserver 0.0.0.0:"$DJANGO_PORT"
