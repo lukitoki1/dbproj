7c9c91923c959c7f5f9cca3ed7dfc7a4fa21b687
Łukasz Kamiński <lukasz.kaminski@empik.com>
2020-05-15 21:23:37 +0200

    connect API schemas to swagger

--- django_con/database_app/urls.py
+++ django_con/database_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+
 from . import views
 from rest_framework import routers
-
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -14,5 +16,14 @@ router.register(r'comments', views.CommentViewSet)
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('', include(router.urls)),
+    path('openapi/', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
