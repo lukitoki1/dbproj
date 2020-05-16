f0842804a5148d4c844680d2a996a96f9c79620f
Łukasz Kamiński <lukasz.kaminski@empik.com>
2020-05-16 17:53:21 +0200

    get hashtag instance by name without "#" prefix in URL

--- django_con/database_app/views.py
+++ django_con/database_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, renderers
 from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .serializers import *
 from .models import *
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -35,6 +38,12 @@ class HashtagViewSet(mixins.CreateModelMixin,
                      viewsets.GenericViewSet):
     queryset = Hashtag.objects.all().order_by('name')
     serializer_class = HashtagSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        instance = Hashtag.objects.get(name="#" + self.kwargs[lookup_url_kwarg])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 
 class ThreadViewSet(viewsets.ModelViewSet):
